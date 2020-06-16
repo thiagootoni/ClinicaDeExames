dcec26bc2644e0da9b6927aaa40a1c06e2c8f23b
Thiago Otoni <thiagootoni_jf@yahoo.com.br>
2020-06-15 21:43:00 -0300

    Estrutura do prejto

---
+++ src/main/java/br/vianna/aula/jsf/ProjectJsfSpringApplication.java
@@ -0,0 +1,13 @@
+package br.vianna.aula.jsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJsfSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJsfSpringApplication.class, args);
+	}
+
+}
